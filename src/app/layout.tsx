1361b0b1d587685e9d7cb076d108362ea9a69da5
EgonBolton <43938777+EgonBolton@users.noreply.github.com>
2024-06-10 17:46:45 -0300

    fix bugs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
+import { authOptions } from "~/server/auth";
+import { getServerSession } from "next-auth/next";
 
 export const metadata = {
   title: "Create T3 App",
@@ -8,14 +10,21 @@ export const metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
+
   return (
     <html lang="en">
-      <body className={GeistSans.className}>{children}</body>
+      <body className={GeistSans.className}>
+        <p>
+          logged in as: {session ? session.user.email : "not logged in"}
+        </p>
+        {children}
+        </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,12 @@
-import Link from "next/link";
-
+"use client";
 import styles from "./index.module.css";
+import { signIn, signOut } from "next-auth/react";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.container}>
-        <h1 className={styles.title}>
-          Create <span className={styles.pinkSpan}>T3</span> App
-        </h1>
-        <div className={styles.cardRow}>
-          <Link
-            className={styles.card}
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className={styles.cardTitle}>First Steps →</h3>
-            <div className={styles.cardText}>
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className={styles.card}
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className={styles.cardTitle}>Documentation →</h3>
-            <div className={styles.cardText}>
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
-      </div>
+      <button onClick={() => signIn("google")}>Sign in with Google</button>
+      <button onClick={() => signOut()}>Sign out</button>
     </main>
   );
 }
